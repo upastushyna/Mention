31e66b3a9581cee6f6af204ad2c93298c24fc6e8
Kostya <k.khairulin@gmail.com>
2018-07-17 23:43:12 +0300

    added back-end skeleton + SpringBootConfigClass + HelloController

---
+++ mentionserver/src/main/java/com.mention/SpringBootConfiguration.java
@@ -0,0 +1,13 @@
+package com.mention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfiguration.class);
+    }
+
+}
