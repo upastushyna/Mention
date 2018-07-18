65f198c3c38ce0ba4114a6301e537368d0e5d62e
feugen55 <feugendanit@gmail.com>
2018-07-18 17:30:58 +0300

    Merge request testing.

--- mentionserver/src/main/java/com.mention/Workflow/StringPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.mention.Workflow;
-
-public class StringPrinter {
-    public static void main(String[] args) {
-        StringMakerOne s1 = new StringMakerOne();
-        StringMakerTwo s2 = new StringMakerTwo();
-        System.out.println(s1.getStringOne() + " " + s2.getStringTwo());
-    }
-}
