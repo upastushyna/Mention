3f9957a68b60c4cefb95392d0797ca1d5d6e86e7
feugen55 <feugendanit@gmail.com>
2018-07-18 13:59:57 +0300

    Test task #1. Create some classes, make a printing main and practice merging.

---
+++ mentionserver/src/main/java/com.mention/Workflow/StringMakerOne.java
@@ -0,0 +1,8 @@
+package com.mention.Workflow;
+
+public class StringMakerOne {
+    public String getStringOne() {
+        String firstString = "He often seeks a lordly dish";
+        return firstString;
+    }
+}
---
+++ mentionserver/src/main/java/com.mention/Workflow/StringMakerTwo.java
@@ -0,0 +1,11 @@
+package com.mention.Workflow;
+
+public class StringMakerTwo {
+    public String getStringTwo(){
+        String secondString = "to serve upon his table.";
+        return secondString;
+    }
+}
+
+
+
