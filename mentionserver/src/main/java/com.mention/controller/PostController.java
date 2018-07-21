c02f924b3174656f8a86f7c09f5c42b4aedf23d5
Yurii Pastushyna <30407072+upastushyna@users.noreply.github.com>
2018-07-21 03:07:47 +0300

    Update PostController.java

--- mentionserver/src/main/java/com.mention/controller/PostController.java
+++ mentionserver/src/main/java/com.mention/controller/PostController.java
@@ -4,7 +4,14 @@ import com.mention.model.Post;
 import com.mention.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/post")
