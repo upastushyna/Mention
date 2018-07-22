9a44dffdecdd8cefce1b9d8efb5cd21ffeb4672a
Oleksandr <o.rodiuk@gmail.com>
2018-07-22 21:41:42 +0300

    404 page react

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+
+import React, { Fragment } from 'react'
+import Body from './Body'
+
+export default class HomePage extends React.Component {
+  render () {
+    return (
+      <Fragment>
+        <Body/>
+      </Fragment>
+    )
+  }
+}
