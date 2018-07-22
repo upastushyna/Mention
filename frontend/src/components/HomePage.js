ff7884e987b01699e89475a8e26f07e775622492
Oleksandr <o.rodiuk@gmail.com>
2018-07-22 22:39:46 +0300

    404 page react - deleted <Fragment/> from Homepage component

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,13 +1,11 @@
 
-import React, { Fragment } from 'react'
+import React from 'react'
 import Body from './Body'
 
 export default class HomePage extends React.Component {
   render () {
     return (
-      <Fragment>
-        <Body/>
-      </Fragment>
+      <Body/>
     )
   }
 }
