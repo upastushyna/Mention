fa6233362b802829569d4390215d47e11a6a7351
Michael Hryhoriev <38781642+MHryhoriev@users.noreply.github.com>
2018-07-25 10:10:39 +0300

    Created entity Favorite (#40)
    * Created entity Favorite
    
    * simple clean code
    
    * Update Favorite.java
    
    * Update Favorite.java

---
+++ mentionserver/src/main/java/com.mention/model/Favorite.java
@@ -0,0 +1,28 @@
+package com.mention.model;
+
+import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+public class Favorite {
+  @Id
+  @Column(name = "favorite_id")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "post_id", nullable = false, updatable = false)
+  private Post post;
+}
