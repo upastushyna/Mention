cd80ef50c7cf4976d9e0665680a9bd3d2211e345
Yurii Pastushyna <30407072+upastushyna@users.noreply.github.com>
2018-07-26 21:03:01 +0300

    Update ProfileService.java

--- mentionserver/src/main/java/com.mention/service/ProfileService.java
+++ mentionserver/src/main/java/com.mention/service/ProfileService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface ProfileService {
 
-  void addProfile (Profile profile);
+  void addProfile(Profile profile);
 
   Optional<Profile> getProfile(Long id);
 
