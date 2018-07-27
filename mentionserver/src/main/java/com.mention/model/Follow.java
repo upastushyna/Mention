88077ee3f273835fd5e6ec58950bc9ba23c1058c
Oleksandr <o.rodiuk@gmail.com>
2018-07-27 18:42:48 +0300

    changed database table column naming

--- mentionserver/src/main/java/com.mention/model/Follow.java
+++ mentionserver/src/main/java/com.mention/model/Follow.java
@@ -10,9 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
@@ -22,25 +20,25 @@ import java.util.Date;
 public class Follow {
 
   @Id
-  @Column(name = "follow_id")
+  @Column(name = "fw_id")
   @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 
   @ManyToOne
-  @JoinColumn(name = "follower_id")
+  @JoinColumn(name = "fw_follower")
   @JsonIgnoreProperties(value =
           {"profile", "sentMessages", "posts", "comments", "receivedMessages", "chats", "favorites", "followers", "signers"})
   private User follower;
 
   @ManyToOne
-  @JoinColumn(name = "signer_id")
+  @JoinColumn(name = "fw_signer")
   @JsonIgnoreProperties(value =
           {"profile", "sentMessages", "posts", "comments", "receivedMessages", "chats", "favorites", "followers", "signers"})
   private User signer;
 
   @CreatedDate
   @Temporal(TemporalType.TIMESTAMP)
-  @Column(nullable = false, name = "follow_timestamp", updatable = false)
+  @Column(nullable = false, name = "fw_timestamp", updatable = false)
   private Date timestamp;
 
 
