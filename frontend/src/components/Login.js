4d3af41f6e34b2a6ae44666cc87da41c072070df
Misha Grygoriev <mgrygoriev@l320st09.di.lan>
2018-07-28 15:34:08 +0300

    Fixed BEM names

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/index.css'
 import logo from '../img/user-icon.png'
@@ -6,22 +6,20 @@ import logo from '../img/user-icon.png'
 export default class Login extends React.Component {
     render () {
         return (
-            <Fragment>
-                <section className="login d-flex">
-                    <div className="login__container">
-                        <div className="login__container--icon">
-                            <img className="login__container--img" src={logo} alt=""/>
-                        </div>
-                        <h1 className="login__container--title">Member Login</h1>
-                        <div className="login__form">
-                            <input type="text" className="login__form--name" placeholder="Username"/>
-                            <input type="password" className="login__form--password" placeholder="Password"/>
-                            <input type="submit" className="login__form--btn" value="login"/>
-                        </div>
-                        <Link to="/" className="login__container--forgot">Forgot Password?</Link>
+            <section className="login d-flex">
+                <div className="login__container">
+                    <div className="login__container--icon">
+                        <img className="login__container--img" src={logo} alt=""/>
                     </div>
-                </section>
-            </Fragment>
+                    <h1 className="login__container--title">Member Login</h1>
+                    <div className="login__form">
+                        <input type="text" className="login__input" placeholder="Username"/>
+                        <input type="password" className="login__input" placeholder="Password"/>
+                        <input type="submit" className="login__btn" value="login"/>
+                    </div>
+                    <Link to="/" className="login__container--forgot">Forgot Password?</Link>
+                </div>
+            </section>
         )
     }
 }
