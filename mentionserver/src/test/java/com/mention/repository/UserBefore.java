f8f6357ecc96fa6ed1bd4869bf82b0740649bede
Yurii Pastushyna <30407072+upastushyna@users.noreply.github.com>
2018-08-02 19:58:44 +0300

    refactor UserBefore class (#68)

--- mentionserver/src/test/java/com/mention/repository/UserBefore.java
+++ mentionserver/src/test/java/com/mention/repository/UserBefore.java
@@ -2,21 +2,22 @@ package com.mention.repository;
 
 import com.mention.model.User;
 import org.junit.Assert;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-
 
 public class UserBefore {
-  public static void createNewUser(UserRepository userRepository) {
-    Assert.assertNull(userRepository.findByUsername("username"));
+  private final UserRepository userRepository;
+
+  public UserBefore(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public void createNewUser(String name, String passwd, String email, boolean userActive) {
+    Assert.assertNull(userRepository.findByUsername(name));
     User user = new User();
-    user.setUsername("username");
-    user.setActive(true);
-    user.setEmail("john@mail.com");
-    user.setPassword("123");
+    user.setUsername(name);
+    user.setActive(userActive);
+    user.setEmail(email);
+    user.setPassword(passwd);
     userRepository.save(user);
-    Assert.assertNotNull(userRepository.findByUsername("username"));
-    //sksfse
+    Assert.assertNotNull(userRepository.findByUsername(name));
   }
 }
