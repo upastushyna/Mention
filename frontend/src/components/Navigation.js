7fea7b118da4d71a41276e8d6c542b9da42ca53b
Misha Grygoriev <mgrygoriev@term09-1.di.lan>
2018-08-04 11:37:01 +0300

    Update some styles

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -15,40 +15,40 @@ class Navigation extends React.Component {
   render () {
     return (
       <Fragment>
-        <div className="header-panel d-flex content-between">
-          <div className="header-panel__left d-flex">
-            <div className="header-panel__logo">
-              <Link to="/" ><img className="header-panel__img" src={logo} alt=""/></Link>
+        <div className="header d-flex content-between">
+          <div className="header__left d-flex">
+            <div className="header__logo">
+              <Link to="/" ><img className="header__img" src={logo} alt=""/></Link>
             </div>
-            <h2 className="header-panel__title">Component name</h2>
-            <form action="" className="header-panel__search">
-              <input type="text" className="header-panel__input" placeholder="Search here people or pages..."/>
-              <input type="submit" className="header-panel__btn" value="Search"/>
+            <h2 className="header__title">Component name</h2>
+            <form action="" className="search">
+              <input type="text" className="search__input search__non-line" placeholder="Search here people or pages..."/>
+              <input type="submit" className="search__btn search__non-line" value="Search"/>
             </form>
           </div>
-          <div className="header-panel__right d-flex">
-            <div className="header-panel__menu">
-              <ul className="header-panel__list d-flex">
-                <li className="header-panel__item">
+          <div className="header__right d-flex">
+            <div className="header__menu">
+              <ul className="header__list d-flex">
+                <li className="header__item">
                   <Link to="/">
-                    <img src={event} alt="" className="header-panel__icon"/>
+                    <img src={event} alt="" className="header__icon"/>
                   </Link>
                 </li>
-                <li className="header-panel__item">
+                <li className="header__item">
                   <Link to="/">
-                    <img src={chat} alt="" className="header-panel__icon"/>
+                    <img src={chat} alt="" className="header__icon"/>
                   </Link>
                 </li>
-                <li className="header-panel__item">
+                <li className="header__item">
                   <Link to="/">
-                    <img src={notification} alt="" className="header-panel__icon"/>
+                    <img src={notification} alt="" className="header__icon"/>
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="header-panel__profile d-flex">
-              <img src={user} alt="" className="header-panel__profile-img"/>
-              <h2 className="header-panel__profile-name">Mykhail Hryhoriev</h2>
+            <div className="header__profile d-flex">
+              <img src={user} alt="" className="header__profile-img"/>
+              <h2 className="header__profile-name">Mykhail Hryhoriev</h2>
             </div>
           </div>
         </div>
