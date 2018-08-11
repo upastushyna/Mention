aba85cbcca5d942b242473099a70e6268e49bf1e
kKhairulin <41304186+kKhairulin@users.noreply.github.com>
2018-08-11 16:13:45 +0300

    added deleting like functionality (#97)

--- frontend/src/containers/LikeItem.js
+++ frontend/src/containers/LikeItem.js
@@ -14,10 +14,20 @@ const LikeItem = props => {
       },
       body: JSON.stringify({user:{id:ID}, post:{id:props.postId}})
     }).then(() => props.loadData(props.username));
-  
+
+  const deleteLike = () => fetch('/api/postlikes/delete',
+    {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({user:{id:ID}, post:{id:props.postId}})
+    }).then(() => props.loadData(props.username));
+
   return <div className="post__like-icon d-flex items-center">
     {props.likes.find(like => like.user.username === USERNAME)?
-    <img src={likeFilled} alt="" className="post__like-img"/>:
+    <img onClick={() => deleteLike()} src={likeFilled} alt="" className="post__like-img"/>:
     <img onClick={() => addLike()} src={like} alt="" className="post__like-img"/>
       }
     <span className="post__like-number">
