592dc3ec5d0975dd16f8365a7a02d5513fd4b572
Yurii Pastushyna <30407072+upastushyna@users.noreply.github.com>
2018-08-14 21:43:29 +0300

    Update ChatDtoRq.java

--- mentionserver/src/main/java/com.mention/dto/ChatDtoRq.java
+++ mentionserver/src/main/java/com.mention/dto/ChatDtoRq.java
@@ -6,10 +6,8 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class ChatDtoRq {
 
-  @Length(min = 3, max = 20)
   private UserDtoUsernameRq user1;
 
-  @Length(min = 3, max = 20)
   private UserDtoUsernameRq user2;
 
 }
