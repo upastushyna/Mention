8e92790fc4241a974828d8b38a7eeb36d4635583
Kostya <k.khairulin@gmail.com>
2018-08-15 16:39:32 +0300

    User page now properly displays user's avatar and background url

--- frontend/src/containers/HeaderProfile.js
+++ frontend/src/containers/HeaderProfile.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import user from '../img/header-panel/user-img.png'
 
-const HeaderProfile = props =>
-      <Fragment>
-        <div className="header-profile">
-          <div className="header-profile__background" />
-          <div className="header-profile__user">
-            <img src={user} alt="" className="header-profile__img"/>
-            <h3 className="header-profile__name">Mykhail Hryhoriev</h3>
-            <h4 className="header-profile__location">Kyiv, Ukraine</h4>
-          </div>
-        </div>
-      </Fragment>
+const HeaderProfile = props => {
+
+  if(!props.user || !props.user.username) {
+    return "loading..."
+  }
+
+  return <Fragment>
+    <div className="header-profile">
+      <img src={props.user.profile.backgroundUrl} className="header-profile__background"/>
+      <div className="header-profile__user">
+        <img src={props.user.profile.avatarUrl} alt="" className="header-profile__img"/>
+        <h3 className="header-profile__name">{props.user.username}</h3>
+        <h4 className="header-profile__location">Kyiv, Ukraine</h4>
+      </div>
+    </div>
+  </Fragment>
+}
 
 export default HeaderProfile
--- mentionserver/src/main/java/com.mention/service/UserServiceImpl.java
+++ mentionserver/src/main/java/com.mention/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mention.service;
 
+import com.mention.dto.ShortUserDetailsRs;
 import com.mention.dto.UserDtoIdRq;
 import com.mention.dto.UserDtoRq;
 import com.mention.repository.UserRepository;
@@ -17,9 +18,22 @@ public class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
 
+  private ModelMapper modelMapper;
+
   @Autowired
   public UserServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
+    this.modelMapper = new ModelMapper();
+  }
+
+  @Override
+  public ShortUserDetailsRs getUser(String username) {
+    Optional<User> user = userRepository.findByUsername(username);
+    if (user.isPresent()) {
+      ShortUserDetailsRs currentUser = modelMapper.map(user.get(), ShortUserDetailsRs.class);
+      return currentUser;
+    }
+    return null;
   }
 
   @Override
