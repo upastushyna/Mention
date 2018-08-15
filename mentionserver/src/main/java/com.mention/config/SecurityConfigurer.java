4353cbcb0c27fd17f2fe9e2fe39577f57f4f99ce
Alexey <alexey.rykhalskiy@gmail.com>
2018-08-15 21:33:40 +0300

    Security (#118)
    * spring security added
    
    * gitignore edited
    
    * Update SecurityConfigurer.java
    
    * Update SecurityConfigurer.java
    
    * identation
    
    * Update SecurityConfigurer.java

---
+++ mentionserver/src/main/java/com.mention/config/SecurityConfigurer.java
@@ -0,0 +1,29 @@
+package com.mention.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+
+        .authorizeRequests()
+        .antMatchers("/*").permitAll()
+        /*
+                .antMatchers("/login/**").anonymous() // or .permitAll()
+                .antMatchers("/reg*").permitAll()
+                .antMatchers("/ul/**").permitAll()
+                .antMatchers("/l/**","/").authenticated()
+        */
+        .and()
+
+    ;
+
+
+  }
+}
