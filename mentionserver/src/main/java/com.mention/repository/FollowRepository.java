958ec10efab6adab478134237d4b2b6f8734c278
Oleksandr <o.rodiuk@gmail.com>
2018-08-17 14:22:40 +0300

    added methods addFolow and deleteFollow in service and controller

--- mentionserver/src/main/java/com.mention/repository/FollowRepository.java
+++ mentionserver/src/main/java/com.mention/repository/FollowRepository.java
@@ -1,9 +1,11 @@
 package com.mention.repository;
 
+import com.mention.dto.UserDtoIdRq;
 import com.mention.model.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+  void deleteByFollowedUser_Id(Long id);
 }
