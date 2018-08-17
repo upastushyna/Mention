6bd1a24541d663866b50d7fbe0de3a8250811fd0
Yurii Pastushyna <30407072+upastushyna@users.noreply.github.com>
2018-08-17 21:42:45 +0300

    Upast: follow unfollow 1.0 (#130)
    * added FollowDto, addFollow, removeFollow
    
    * added methods addFolow and deleteFollow in service and controller
    
    * сhanged method removeFollow in service
    
    * сhanged methods in service and controller
    
    * added FollowDto, addFollow, removeFollow
    
    * added methods addFolow and deleteFollow in service and controller
    
    * сhanged method removeFollow in service
    
    * сhanged methods in service and controller
    
    * fix Follow Service add and delete

--- mentionserver/src/main/java/com.mention/controller/FollowController.java
+++ mentionserver/src/main/java/com.mention/controller/FollowController.java
@@ -1,12 +1,19 @@
 package com.mention.controller;
 
+import com.mention.dto.FollowDtoRq;
 import com.mention.dto.ShortUserDetailsRs;
+import com.mention.dto.UserDtoIdRq;
+import com.mention.model.Follow;
 import com.mention.service.UserFollowServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -29,4 +36,16 @@ public class FollowController {
   List<ShortUserDetailsRs> getFollowingUsers(@PathVariable String username) {
     return followService.getFollowingUsers(username);
   }
+
+  @PostMapping("/add")
+  public void addFollow(@RequestBody FollowDtoRq follow) {
+    followService.addFollow(follow);
+
+  }
+
+  @DeleteMapping("/delete")
+  public void deleteFollow(@RequestBody FollowDtoRq follow) {
+    followService.deleteFollow(follow);
+  }
 }
+
