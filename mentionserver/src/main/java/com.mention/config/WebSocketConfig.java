fa9456131c985bb936f02732d8ef01d424b6f32b
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-08-20 20:30:08 +0300

    websocket

---
+++ mentionserver/src/main/java/com.mention/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.mention.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // front broker registration to send messages to front
+        config.enableSimpleBroker("/front");
+        // back endpoint prefix to obtain messages from front
+        config.setApplicationDestinationPrefixes("/back");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // back registration to obtain messages from front
+        registry.addEndpoint("/ws_0001").withSockJS();
+    }
+
+}
---
+++ mentionserver/src/main/java/com.mention/dto/WsRqDto.java
@@ -0,0 +1,25 @@
+package com.mention.dto;
+
+public class WsRqDto {
+
+    private String name;
+
+    protected WsRqDto() {}
+
+    public WsRqDto(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("RqDto{name='%s'}", name);
+    }
+}
