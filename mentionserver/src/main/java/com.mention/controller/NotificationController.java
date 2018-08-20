7a543d18022ab649c6cd3471a6461033b8c93718
Kostya <k.khairulin@gmail.com>
2018-08-20 21:57:24 +0300

    initial

---
+++ mentionserver/src/main/java/com.mention/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package com.mention.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+  
+}
