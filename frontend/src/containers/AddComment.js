3851dc85601bc488f69679f399c987d647509382
Yurii Pastushyna <upastushyna@gmail.com>
2018-08-27 18:56:50 +0300

    fix minor

--- frontend/src/containers/AddComment.js
+++ frontend/src/containers/AddComment.js
@@ -17,8 +17,6 @@ const AddComment = props => {
     .then(() => document.getElementById('commentInput' + props.postId).value = '')
 
   return <Fragment>
-<<<<<<< HEAD
-   
       <div className="add-comment">
         <img src={props.currentUser.profile.avatarUrl} alt="avatar" className="add-comment__avatar"/>
         <textarea className="create-post__input"
@@ -27,18 +25,7 @@ const AddComment = props => {
           maxLength={280} />
           <button onClick={() => addComment()} className="create-post__btn">Add</button>
           <button onClick={() => addComment()} className="create-post__btn create-post__btn_rounded">+</button>
-=======
-    <div className="add-comment white-background">
-      <div className="add-comment__form d-flex items-center">
-        <img src={props.currentUser.profile.avatarUrl} alt="" className="add-comment__avatar"/>
-        <textarea className="add-comment__input"
-                  onKeyPress={(e) => {(e.key === 'Enter' ?  addComment(): null)}}
-          id={'commentInput' + props.postId} placeholder="Share your thoughts"
-          maxLength={280}/>
->>>>>>> d8e00c96d10c7e2ac95d94980b782fd7b1d5b12c
       </div>
-    
-    
   </Fragment>
 }
 
