7cf231896e9aebde010067e5db22bb482f7fa279
kKhairulin <41304186+kKhairulin@users.noreply.github.com>
2018-08-27 16:20:48 +0300

    Update LoginServiceImpl.java

--- mentionserver/src/main/java/com.mention/service/LoginServiceImpl.java
+++ mentionserver/src/main/java/com.mention/service/LoginServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import sun.tools.jstat.Token;
 import java.util.Optional;
 import java.util.UUID;
 
