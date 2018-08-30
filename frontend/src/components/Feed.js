49163943a95e603f3f9769e2f7b3ecb185f99c29
kKhairulin <41304186+kKhairulin@users.noreply.github.com>
2018-08-30 17:11:42 +0300

    Khairulin 0.1 front header and scroller (#159)
    * Added authorization on back-end.
    
    * checkstyle fix
    
    * Front-end login + fixes
    
    * Fixes + config.properties update
    
    * Registration implementation on back-end. In proccess
    
    * Registration implementation on back-end. In proccess
    
    * blah
    
    * Implemented login and registration on front and back-end
    
    * minor fixes on front and back -end
    
    * Code refactoring
    
    * checkstyle fix
    
    * fixes
    
    * add
    
    * fixed imports
    
    * Small fixes on front-end
    
    * scroll to top button and dropdown profile+logout

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -21,6 +21,21 @@ class Feed extends React.Component {
     }
   }
 
+  handleOnScroll = () => {
+    if(window.pageYOffset > 260 && this.refs.scroller) {
+      this.refs.scroller.classList.remove("d-none");
+    }
+  };
+
+  scrollToTop = () => {
+    this.refs.pageTop.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+    this.refs.scroller.classList.add("d-none");
+  };
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleOnScroll);
+  }
+
   addPost = event => {
     event.preventDefault();
     const data = new FormData();
@@ -58,8 +73,9 @@ class Feed extends React.Component {
 
     return (
       <Fragment>
-        <Navigation/>
-        <div className="container">
+        <div ref="pageTop" style={{ float: 'left', clear: 'both' }}></div>
+        <Navigation history={this.props.history}/>
+        <div ref="container" className="container">
           {this.props.currentUser.followedUsers.find(follow =>
             follow.followedUser.id === this.props.currentUser.id)?
           <div className="create-post">
@@ -77,6 +93,7 @@ class Feed extends React.Component {
               <input onChange={() => this.changeName()} className="upload" id="inputFile" ref="inputFile" type="file"/></div>
             </form>
           </div> : ""}
+          <button onClick={() => this.scrollToTop()} ref="scroller" className="scroll d-none">Scroll to top</button>
           <PostsContainer username={this.props.currentUser.username}
             userPosts={this.props.feed}
             loadData={this.props.loadData}
