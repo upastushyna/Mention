fbcd7f94b5a3f078c16cd7658b5a07d39beffd9b
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2018-08-30 20:46:34 +0300

    Front fixes (#162)
    * editprofile
    
    * fixes

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -6,6 +6,7 @@ import {loadSearchUsers} from '../actions/searchUsersActions'
 import PostsContainer from '../containers/PostsContainer'
 import UsersContainer from '../containers/UsersContainer'
 import {loadCurrentUser} from '../actions/currentUserActions'
+import searchIcon from '../img/search-icon.svg'
 
 class SearchPage extends React.Component {
   componentWillMount () {
@@ -54,7 +55,12 @@ class SearchPage extends React.Component {
       <Fragment>
         <Navigation/>
         <div className="container">
-          {this.props.foundPosts.length === 0 ? 'Sorry, no posts found matching your search'
+          {this.props.foundPosts.length === 0 ?          
+          <div className="empty-state">
+           <img className="empty-state__icon" src={searchIcon} alt="search"/>
+          <h2 className="empty-state__title">Ups! Nothing has been found :( </h2>
+          <p className="empty-state__msg">Please, try another search query</p>
+          </div>       
             : <PostsContainer username={this.props.match.params.input}
               userPosts={this.props.foundPosts}
               loadData={this.props.loadPosts}
