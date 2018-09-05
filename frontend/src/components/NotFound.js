c06ade321ab35c48b46ff2c927e1f4a34600ca20
Misha Grygoriev <mgrygoriev@l320st05.di.lan>
2018-09-05 21:39:18 +0300

    Update styles

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,19 +1,20 @@
 import React, { Fragment } from 'react'
 import {Link} from 'react-router-dom'
+import back from '../img/back-icon.png'
+
 export default class NotFound extends React.Component {
-    render () {
-        return (
-            <Fragment key={NotFound.id}>
-                <section className="notFound">
-                    <div className="notFound__errorText">
-                        <h1 className="notFound__errorHeader">404</h1>
-                        <p className="notFound__errorBody">Oops, the page you are trying to reach does not exist.</p>
-                    </div>
-                    <div>
-                        <Link to="/"><button className="returnButton">Back to main</button></Link>
-                    </div>
-                </section>
-            </Fragment>
-        )
-    }
+  render () {
+    return (
+        <Fragment key={NotFound.id}>
+          <section className="notFound">
+            <div className="notFound__errorText">
+              <p className="notFound__errorBody">Oops, the page you are trying to reach does not exist.</p>
+              <Link to="/">
+                <img src={back} alt="" className="returnButton"/>
+              </Link>
+            </div>
+          </section>
+        </Fragment>
+    )
+  }
 }
