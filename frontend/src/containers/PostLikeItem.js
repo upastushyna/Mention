624b48310d61c1730c3e7a18ee67cbbd55430f5c
ydegtyar <yaroslavdehtiar@gmail.com>
2018-09-06 20:27:08 +0300

    Add dropdown Post and Comment likers list

--- frontend/src/containers/PostLikeItem.js
+++ frontend/src/containers/PostLikeItem.js
@@ -27,26 +27,32 @@ const PostLikeItem = props => {
         body: JSON.stringify({user: {id: props.currentUser.id}, post: {id: props.postId}})
       }).then(() => props.loadData(props.username))
 
-  const show = () => {
-    if (!document.getElementById("likers").classList.contains("d-none")) {
-      hide()
-    } else {
-      document.getElementById("likers").classList.remove("d-none");
-    }
-  }
-
-  const hide = () => {
-    if (document.getElementById("likers")) {
-      document.getElementById("likers").classList.add("d-none");
+  const showOptions = id => {
+    if (document.getElementById('post' + id).classList.contains('d-none')) {
+      document.getElementById('post' + id).classList.remove('d-none')
     }
   };
+
+  const hide = event => {
+    if (!event.target.classList.contains('likers__nav')) {
+      let hideButton = document.getElementsByClassName('likers__nav');
+
+      Array.prototype.forEach.call(hideButton, item => {
+        if(!item.classList.contains('d-nome')) {
+          item.classList.add('d-none')
+        }
+      })
+    }
+  };
+
+  window.addEventListener('mousedown', event => hide(event));
 
   return <div className="d-flex-center">
     {props.likes.find(like => like.user.username === props.currentUser.username)
         ? <img onClick={() => deleteLike()} src={likeFilled} alt="like" className="post__action-img"/>
         : <img onClick={() => addLike()} src={like} alt="dislike" className="post__action-img"/>}
-    <span className="post__action-count" onClick={() => show()}>{props.likes.length}</span>
-    <div id="likers" className="d-none likers__nav">
+    <span className="post__action-count" onClick={() => showOptions(props.postId)}>{props.likes.length}</span>
+    <div id={"post" + props.postId} className="d-none likers__nav">
       <UserLikesContainer likes={props.likes}/>
     </div>
   </div>
