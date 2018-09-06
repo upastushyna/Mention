a7fb8b4cab7d37ebba2edd829255fe8e555466fd
Kostya <k.khairulin@gmail.com>
2018-09-06 13:12:23 +0300

    added notification entity

---
+++ mentionserver/src/main/java/com.mention/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.mention.repository;
+
+import com.mention.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+}
