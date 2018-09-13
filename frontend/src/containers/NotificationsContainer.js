47f9b672b937e72639ebf40223aab501ddf64a1d
Kostya <k.khairulin@gmail.com>
2018-09-13 17:00:56 +0300

    asda [skip ci]

--- frontend/src/containers/NotificationsContainer.js
+++ frontend/src/containers/NotificationsContainer.js
@@ -10,5 +10,4 @@ const NotificationsContainer = props => {
   return props.notifications.map(notification =>
     <NotificationItem notification={notification}/>)
 };
-
 export default NotificationsContainer
