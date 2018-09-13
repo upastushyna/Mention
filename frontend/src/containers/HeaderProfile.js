f0dae8bd93750dd9f07f0a859c1edb5a3159ddd3
IrinaStoetski <30476402+IrinaStoetski@users.noreply.github.com>
2018-09-13 09:42:01 +0300

    Onboard (#211)
    * small fixes
    
    * error fix
    
    * error fix
    
    * 404 + content size fix
    
    * parralax, userlikes fix

--- frontend/src/containers/HeaderProfile.js
+++ frontend/src/containers/HeaderProfile.js
@@ -7,8 +7,7 @@ const HeaderProfile = props => {
   }
 
   return <Fragment>
-    <div className="header-profile">
-      <img src={props.user.profile.backgroundUrl} alt="background" className="header-profile__background"/>
+   <div style={ { backgroundImage: `url(${props.user.profile.backgroundUrl})` } } className="header-profile">
       <div className="header-profile__user">
         <img src={props.user.profile.avatarUrl} alt="avatar" className="header-profile__img"/>
         <h3 className="profile-info__username">{props.user.username}</h3>
--- frontend/src/containers/UserLikeItem.js
+++ frontend/src/containers/UserLikeItem.js
@@ -2,13 +2,11 @@ import React from 'react'
 import {Link} from "react-router-dom";
 
 const UserLikeItem = props =>
-    <div className="follow-container">
-      <div className="d-flex-center">
-        <Link to={'/user/' + props.user.username}>
+    <div className="follow-container">      
+        <Link to={'/user/' + props.user.username} className="d-flex-center">
           <img src={props.user.profile.avatarUrl} alt="avatar" className="profile-info__avatar"/>
           <h2 className="profile-info__username">{props.user.username}</h2>
         </Link>
-      </div>
     </div>
 
 export default UserLikeItem
