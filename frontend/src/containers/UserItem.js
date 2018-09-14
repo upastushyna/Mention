43a37f815ced4916d96a6352cfe06f37e38dc1d6
Shurara <o.rodiuk@gmail.com>
2018-09-14 19:38:32 +0300

    Orodiuk click on post redirect page post (#219)
    * click on post body - redirect on route /post/:id
    
    * checkstyle
    
    * redirect to feed after delete post on page post+ID
    
    * update feed after deleting post on postId page and redirecting to feed
    
    * checkstyle fix

--- frontend/src/containers/UserItem.js
+++ frontend/src/containers/UserItem.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import UnffollowButton from './UnffollowButton'
 import FollowButton from './FollowButton'
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom'
 
 const UserItem = props =>
   <div className="follow-container">
-    <Link to={"/user/" + props.user.username} className="post__link">
+    <Link to={'/user/' + props.user.username} className="post__link">
       <div className="d-flex-center">
         <img src={props.user.profile.avatarUrl} alt="avatar" className="profile-info__avatar"/>
-          <h2 className="profile-info__username">{props.user.username}</h2>
+        <h2 className="profile-info__username">{props.user.username}</h2>
       </div>
     </Link>
-      {props.currentUser.followedUsers.find(follow =>
-        follow.followedUser.id === props.user.id)?
-        <UnffollowButton unfollow={props.unfollow} followedUser={props.user.id}/> :
-        <FollowButton follow={props.follow} followedUser={props.user.id}/>
-      }
-  </div>;
+    {props.currentUser.followedUsers.find(follow =>
+      follow.followedUser.id === props.user.id)
+      ? <UnffollowButton unfollow={props.unfollow} followedUser={props.user.id}/>
+      : <FollowButton follow={props.follow} followedUser={props.user.id}/>
+    }
+  </div>
 
 export default UserItem
