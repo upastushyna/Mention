35073a39c3534f210aeaf9556b5982799e8ecc7f
YaroslavDehtiar <yaroslavdehtiar@gmail.com>
2018-09-16 18:23:36 +0300

    Add timezones

--- frontend/src/js/timestamp.js
+++ frontend/src/js/timestamp.js
@@ -2,12 +2,15 @@ export const getDateFromDb = dateDB => {
   let year = dateDB.substr(0, 4)
   let month = dateDB.substr(5, 2)
   let day = dateDB.substr(8, 2)
-  let hour = dateDB.substr(11, 2)
+  let hour1 = dateDB.substr(11, 2)
   let minute = dateDB.substr(14, 2)
   let second = dateDB.substr(17, 2)
 
-  let time = dateDB.substr(11, 8)
-  let yearMonthDate = dateDB.substr(0, 10)
+  let tz = new Date();
+  let hour = parseInt(hour1) - tz.getTimezoneOffset()/60;
+
+  let time = hour.toString() + ':' + minute + ':' + second;
+  let yearMonthDate = dateDB.substr(0, 10);
 
   let createdDate = new Date(year, month, day, hour, minute, second)
   let currentDate = new Date()
