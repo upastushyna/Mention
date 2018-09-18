aebd1d0c390c97bb66b3fd2f8c21bf062e9f3af3
Kostya <k.khairulin@gmail.com>
2018-09-18 10:52:07 +0300

    Added empty states texts, images

--- frontend/src/containers/EmptyState.js
+++ frontend/src/containers/EmptyState.js
@@ -4,7 +4,7 @@ import searchIcon from '../img/search-icon.svg'
 const EmptyState = props => {
   return (
     <div className="empty-state">
-      <img className="empty-state__icon" src={searchIcon} alt="emptystate"/>
+      <img className="empty-state__icon" src={props.image} alt="emptystate"/>
       <h2 className="empty-state__title">{props.title}</h2>
       <p className="empty-state__msg">{props.message}</p>
     </div>
--- frontend/src/containers/PostsContainer.js
+++ frontend/src/containers/PostsContainer.js
@@ -2,13 +2,14 @@ import React from 'react'
 import PostItem from './PostItem'
 import Loader from "./Loader";
 import EmptyState from "./EmptyState";
-import searchIcon from '../img/search-icon.svg'
 
 const PostsContainer = props => {
   if (props.userPosts.length === 0) {
     return (<div className="empty-loader">
       <Loader/>
-      <EmptyState image={searchIcon} title="Oops! Nothing has been found :(" message={'Please, try another search query'}/>
+      <EmptyState image={props.image}
+                  title={props.title}
+                  message={props.message}/>
     </div>)
   }
 
