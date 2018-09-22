bc9f9463494b8aa4623ca3002cd22b2b0bdc24ed
yaroslavdehtiar <yaroslavdehtiar@gmail.com>
2018-09-22 16:36:47 +0300

    fix

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -12,18 +12,20 @@ export default class Registration extends React.Component {
   login = event => {
     event.preventDefault();
     fetch('/api/login',
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({usernameOrEmail: this.refs.loginUsername.value,
-          password: this.refs.loginPassword.value})
-      }).then(res => res.json())
-      .then(res => res.accessToken ? localStorage.setItem('accessToken', res.accessToken) : this.showMessage(res))
-      .then(() => localStorage.getItem('accessToken')
-        ? setTimeout(() => this.props.history.push('/'), 1000) : null)
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            usernameOrEmail: this.refs.loginUsername.value,
+            password: this.refs.loginPassword.value
+          })
+        }).then(res => res.json())
+        .then(res => res.accessToken ? localStorage.setItem('accessToken', res.accessToken) : this.showMessage(res))
+        .then(() => localStorage.getItem('accessToken')
+            ? setTimeout(() => this.props.history.push('/'), 1000) : null)
   };
 
   showMessage = res => {
@@ -70,29 +72,29 @@ export default class Registration extends React.Component {
       return
     }
     fetch('/api/register',
-      {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username: this.refs.registerUsername.value,
-          email: this.refs.registerEmail.value,
-          password: this.refs.registerPassword.value})
-      }).then(res => res.json())
-      .then(res => res.status === 200 ? this.showSuccess(res) : this.showError(res))
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: this.refs.registerUsername.value,
+            email: this.refs.registerEmail.value,
+            password: this.refs.registerPassword.value
+          })
+        }).then(res => res.json())
+        .then(res => this.showResponse(res))
   };
 
-  showSuccess = res => {
-    this.createValidMessage(res.message, document.getElementById('registerForm'), 0);
-  };
-
-  showError = res => {
+  showResponse = res => {
     if (res.message.indexOf('Validation') === 0) {
       this.createMessage('Please, enter a valid email address',
-        document.getElementById('registerForm'), 0);
-    } else {
+          document.getElementById('registerForm'), 0);
+    } else if (res.message.indexOf('confirmation')) {
       this.createValidMessage(res.message, document.getElementById('registerForm'), 0);
+    } else {
+      this.createMessage(res.message, document.getElementById('registerForm'), 0);
     }
   };
 
@@ -106,53 +108,57 @@ export default class Registration extends React.Component {
     this.refs.loginForm.classList.remove('d-none')
   };
 
-  render () {
+  render() {
     // const {showRegister} = this.state
 
     return (
-      <Fragment key={Registration.id}>
-        <div>
-          <p className='d-none' ref='message'></p>
-        </div>
-        <section ref="registerForm" className="login d-flex-center d-none">
-          <div className="login__container">
-            <img className="login__icon" src={logo} alt="profile"/>
-            <h1 className="login__title">Join the community</h1>
-            <form onSubmit={event => this.register(event)}>
-              <div id='registerForm' className="login__form">
-                <input ref="registerUsername" type="text" className="input_custom" placeholder="Username" minLength="3" maxLength="20"/>
-                {/*<input ref="registerUsername" type="text" className={`input input_text ${showRegister ? 'input_custom' : 'd-none'}`} placeholder="Username" minLength="3" maxLength="20"/>*/}
-                <input ref="registerEmail" type="text" className="input_custom" placeholder="Email"/>
-                <input ref="registerPassword" type="password" className="input_custom" placeholder="Password" minLength="6" maxLength="24"/>
-                <input ref="confirmPassword" type="password" className="input_custom"
-                  placeholder="Confirm Password"/>
-                <input className="btn-action login__btn" type="submit" placeholder="Register"/>
-              </div>
-            </form>
-            <p onClick={() => this.showLogin()} className="login__forgot-password">Member Login</p>
-            {/*<p onClick={() => this.setState({showRegister: true})} className="login__forgot-password">Member Login</p>*/}
+        <Fragment key={Registration.id}>
+          <div>
+            <p className='d-none' ref='message'></p>
           </div>
-        </section>
-        <section ref="loginForm" className="login d-flex-center">
-          <div className="login__container">
+          <section ref="registerForm" className="login d-flex-center d-none">
+            <div className="login__container">
+              <img className="login__icon" src={logo} alt="profile"/>
+              <h1 className="login__title">Join the community</h1>
+              <form onSubmit={event => this.register(event)}>
+                <div id='registerForm' className="login__form">
+                  <input ref="registerUsername" type="text" className="input_custom" placeholder="Username"
+                         minLength="3" maxLength="20"/>
+                  {/*<input ref="registerUsername" type="text" className={`input input_text ${showRegister ? 'input_custom' : 'd-none'}`} placeholder="Username" minLength="3" maxLength="20"/>*/}
+                  <input ref="registerEmail" type="text" className="input_custom" placeholder="Email"/>
+                  <input ref="registerPassword" type="password" className="input_custom" placeholder="Password"
+                         minLength="6" maxLength="24"/>
+                  <input ref="confirmPassword" type="password" className="input_custom"
+                         placeholder="Confirm Password"/>
+                  <input className="btn-action login__btn" type="submit" placeholder="Register"/>
+                </div>
+              </form>
+              <p onClick={() => this.showLogin()} className="login__forgot-password">Member Login</p>
+              {/*<p onClick={() => this.setState({showRegister: true})} className="login__forgot-password">Member Login</p>*/}
+            </div>
+          </section>
+          <section ref="loginForm" className="login d-flex-center">
+            <div className="login__container">
 
-            <img className="login__icon" src={logo} alt="login"/>
+              <img className="login__icon" src={logo} alt="login"/>
 
-            <h1 className="login__title">Login</h1>
-            <form id='loginForm' onSubmit={event => this.login(event)}>
-              <input ref="loginUsername" type="text" className="input_custom" placeholder="Username or Email" minLength="3"/>
-              <input ref="loginPassword" type="password" className="input_custom" placeholder="Password" minLength="6" maxLength="24"/>
-              <input type="submit" className="btn-action login__btn" value="login"/>
-            </form>
-            <button onClick={() => this.showRegister()} className="login__sign-btn">Sign up</button>
-            <Link to="/" className="login__forgot-password">Forgot Password?</Link>
-          </div>
-        </section>
-        <Switch>
-          <Route path='/registration/:userToken' component={props =>
-            <RegisterConfirmation userToken={props.match.params.userToken}/>}/>
-        </Switch>
-      </Fragment>
+              <h1 className="login__title">Login</h1>
+              <form id='loginForm' onSubmit={event => this.login(event)}>
+                <input ref="loginUsername" type="text" className="input_custom" placeholder="Username or Email"
+                       minLength="3"/>
+                <input ref="loginPassword" type="password" className="input_custom" placeholder="Password" minLength="6"
+                       maxLength="24"/>
+                <input type="submit" className="btn-action login__btn" value="login"/>
+              </form>
+              <button onClick={() => this.showRegister()} className="login__sign-btn">Sign up</button>
+              <Link to="/" className="login__forgot-password">Forgot Password?</Link>
+            </div>
+          </section>
+          <Switch>
+            <Route path='/registration/:userToken' component={props =>
+                <RegisterConfirmation userToken={props.match.params.userToken}/>}/>
+          </Switch>
+        </Fragment>
 
     )
   }
