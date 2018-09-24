d8eaca11cf384bb9989f5b9d215f0c075f7c6630
kKhairulin <41304186+kKhairulin@users.noreply.github.com>
2018-09-24 13:19:26 +0300

    Added forgot password form and back-end logic to send an email (#251)
    * Added forgot password form and back-end logic to send an email with token
    
    * checkstyle fix

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -5,9 +5,6 @@ import '../css/index.css'
 import logo from '../img/posts-icon.png'
 
 export default class Registration extends React.Component {
-  state = {
-    /*showRegister: false*/
-  };
 
   login = event => {
     event.preventDefault();
@@ -44,7 +41,7 @@ export default class Registration extends React.Component {
   displayValidMessage = () => {
     this.refs.message.classList.remove('d-none');
     this.refs.message.classList.add('login__valid');
-  }
+  };
 
   setMessage = message => {
     this.refs.message.innerText = message;
@@ -63,7 +60,23 @@ export default class Registration extends React.Component {
     this.setMessage(message);
     this.displayValidMessage();
     this.insertMessage(el, index)
-  }
+  };
+
+  recoverPassword = event => {
+    event.preventDefault();
+    fetch('/api/recover',
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: this.refs.forgotEmail.value
+        })
+      }).then(res => res.json())
+      .then(res => this.showResponse(res, document.getElementById('forgotForm')))
+  };
 
   register = event => {
     event.preventDefault();
@@ -84,17 +97,18 @@ export default class Registration extends React.Component {
             password: this.refs.registerPassword.value
           })
         }).then(res => res.json())
-        .then(res => this.showResponse(res))
+        .then(res => this.showResponse(res, document.getElementById('registerForm')))
   };
 
-  showResponse = res => {
-    if (res.message.indexOf('Validation') === 0) {
+  showResponse = (res, el) => {
+    console.log(res.message);
+    if (res.message.indexOf('Validation') >= 0) {
       this.createMessage('Please, enter a valid email address',
-          document.getElementById('registerForm'), 0);
-    } else if (res.message.indexOf('confirmation')) {
-      this.createValidMessage(res.message, document.getElementById('registerForm'), 0);
+          el, 0);
+    } else if (res.message.indexOf('confirmation') >= 0) {
+      this.createValidMessage(res.message, el, 0);
     } else {
-      this.createMessage(res.message, document.getElementById('registerForm'), 0);
+      this.createMessage(res.message, el, 0);
     }
   };
 
@@ -105,7 +119,13 @@ export default class Registration extends React.Component {
 
   showLogin = () => {
     this.refs.registerForm.classList.add('d-none');
-    this.refs.loginForm.classList.remove('d-none')
+    this.refs.forgotForm.classList.add('d-none');
+    this.refs.loginForm.classList.remove('d-none');
+  };
+
+  showForgot = () => {
+    this.refs.loginForm.classList.add('d-none');
+    this.refs.forgotForm.classList.remove('d-none');
   };
 
   render() {
@@ -133,8 +153,7 @@ export default class Registration extends React.Component {
                   <input className="btn-action login__btn" type="submit" placeholder="Register"/>
                 </div>
               </form>
-              <p onClick={() => this.showLogin()} className="login__forgot-password">Member Login</p>
-              {/*<p onClick={() => this.setState({showRegister: true})} className="login__forgot-password">Member Login</p>*/}
+              <button onClick={() => this.showLogin()} className="login__sign-btn">Member Login</button>
             </div>
           </section>
           <section ref="loginForm" className="login d-flex-center">
@@ -151,10 +170,25 @@ export default class Registration extends React.Component {
                 <input type="submit" className="btn-action login__btn" value="login"/>
               </form>
               <button onClick={() => this.showRegister()} className="login__sign-btn">Sign up</button>
-              <Link to="/" className="login__forgot-password">Forgot Password?</Link>
+              <button onClick={() => this.showForgot()} className="login__forgot-password">Forgot Password?</button>
+            </div>
+          </section>
+          <section ref="forgotForm" className="login d-flex-center d-none">
+            <div className="login__container">
+
+              <img className="login__icon" src={logo} alt="login"/>
+
+              <h1 className="login__title">Enter your email address</h1>
+              <form id='forgotForm' onSubmit={event => this.recoverPassword(event)}>
+                <input ref="forgotEmail" type="text" className="input_custom" placeholder="Email address"
+                       minLength="3"/>
+                <input type="submit" className="btn-action login__btn" value="Send email"/>
+              </form>
+              <button onClick={() => this.showLogin()} className="login__sign-btn">Sign in</button>
             </div>
           </section>
           <Switch>
+            <Route path='/registration/recover/:userToken'/>
             <Route path='/registration/:userToken' component={props =>
                 <RegisterConfirmation userToken={props.match.params.userToken}/>}/>
           </Switch>
